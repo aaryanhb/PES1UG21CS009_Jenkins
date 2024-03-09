a14e8c43d3537562c4ba8e410f76be72aec0261d
Aaryan H Badyal <93147402+aaryanhb@users.noreply.github.com>
2024-03-09 12:45:06 +0530

    changes

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "Hello, changes have been made\n";
+  cout << "Hello, changes have been made by aaryan\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
